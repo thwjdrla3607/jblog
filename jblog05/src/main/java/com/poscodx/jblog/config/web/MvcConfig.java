b45ad41169009d8624f86ae27c5af533ab03c3eb
sso <sojeong7524@gmail.com>
2023-10-16 17:52:27 +0900

    jblog05 #finish

--- jblog05/src/main/java/com/poscodx/jblog/config/web/MvcConfig.java
+++ jblog05/src/main/java/com/poscodx/jblog/config/web/MvcConfig.java
@@ -3,8 +3,8 @@ package com.poscodx.jblog.config.web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -14,8 +14,10 @@ import org.springframework.web.servlet.view.JstlView;
 public class MvcConfig implements WebMvcConfigurer{
 	
 	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-		configurer.enable();
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+			.addResourceHandler("/assets/**")
+			.addResourceLocations("classpath:assets/");
 	}
 	
 	@Bean
@@ -27,4 +29,9 @@ public class MvcConfig implements WebMvcConfigurer{
 		
 		return viewResolver;
 	}
+	
+//	@Override
+//	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+//		configurer.enable();
+//	}
 }
--- jblog05/src/main/java/com/poscodx/jblog/exception/GlobalExceptionHandler.java
+++ jblog05/src/main/java/com/poscodx/jblog/exception/GlobalExceptionHandler.java
@@ -8,19 +8,25 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	private static final Log logger = LogFactory.getLog(GlobalExceptionHandler.class);
-	
+
 	@ExceptionHandler(Exception.class)
 	public String handlerException(Model model, Exception e) {
-		//1. 로깅(Logging)
+		// 1. 404 Error 처리
+		if (e instanceof NoHandlerFoundException) {
+			return "error/404";
+		}
+
+		// 2. 로깅(Logging)
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
 		logger.error(errors.toString());
-		
-		//2. 사과 페이지
+
+		// 3. 사과 페이지
 		model.addAttribute("errors", errors.toString());
 		return "error/exception";
 	}
