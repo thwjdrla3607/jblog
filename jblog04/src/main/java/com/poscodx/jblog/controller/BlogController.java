668c417de2ec1c4ff0ccd71b9dc0589f5e035936
sso <sojeong7524@gmail.com>
2023-10-16 16:29:20 +0900

    jblo04 #post 수정

--- jblog04/src/main/java/com/poscodx/jblog/controller/BlogController.java
+++ jblog04/src/main/java/com/poscodx/jblog/controller/BlogController.java
@@ -162,7 +162,7 @@ public class BlogController {
 	public String adminWrite(@PathVariable("id")String blogId,
 							@AuthUser UserVo userVo,
 							PostVo postVo,
-	                        @RequestParam(value="category", required=true, defaultValue="") String categoryNo,
+	                        @RequestParam(value="category", required=true, defaultValue="") int categoryNo,
 							Model model) {
 		if (!blogId.equals(userVo.getId())) {
 			return "main/index";
@@ -173,3 +173,4 @@ public class BlogController {
 		return "redirect:/" + blogId;
 	}
 }
+	
